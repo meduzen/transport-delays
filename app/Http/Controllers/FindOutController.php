1556303813eb2e905c589dbfff655215063fd7c7
Mehdi M <hi@mehdi.cc>
2024-05-10 04:18:19 +0200

    Update discovery controller

--- app/Http/Controllers/FindOutController.php
+++ app/Http/Controllers/FindOutController.php
@@ -2,25 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stop;
 use Illuminate\Support\Facades\Storage;
+
+// https://stackoverflow.com/questions/24689852/fetching-mysql-geo-point-data-and-storing-result-in-php-variable
+function rawPointToFloatPair($data)
+{
+    $res = unpack("lSRID/CByteOrder/lTypeInfo/dX/dY", $data);
+    return [$res['X'],$res['Y']];
+}
 
 class FindOutController extends Controller
 {
     public function __invoke()
     {
-        $stop_details = collect(Storage::json('samples/stop-details-production.json'))
-            ->map(fn ($stop) => [
-                'id' => $stop['id'],
-                'name' => json_decode($stop['name']),
-                'gpscoordinates' => json_decode($stop['gpscoordinates']),
-            ]);
+        $stops = Stop::all()
+            ->map(function ($stop){
+                $stop->name = json_decode($stop->name);
+                $stop->coordinates = rawPointToFloatPair($stop->coordinates);
+                return $stop;
+            });
+
 
         $lines = collect(Storage::json('seed/stib-stops-by-line-production.json'))
-            ->map(function ($line) use ($stop_details) {
+            ->map(function ($line) use ($stops) {
                 $lines_stop = collect(json_decode($line['points']))->sortBy('order');
 
-                $lines_stop = $lines_stop->each(function ($stop) use ($stop_details) {
-                    $matching_stop = $stop_details->first(fn ($stop_detail) => $stop_detail['id'] == $stop->id);
+                $lines_stop = $lines_stop->each(function ($stop) use ($stops) {
+                    $matching_stop = $stops->first(fn ($stop_detail) => $stop_detail->internal_id == $stop->id);
                     if ($matching_stop) {
                         $stop->name = $matching_stop['name'];
                     } else {
