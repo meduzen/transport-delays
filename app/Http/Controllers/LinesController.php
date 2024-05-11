c594e6e53c8539ee118a6582f89538008611a33c
Mehdi M <hi@mehdi.cc>
2024-05-11 12:04:10 +0200

    Properly sort lines
    This is better doing at MySQL level for performance reasons, but the sorting is string-based (e.g. `'13'` comes before `'2'`) while it’s doing it as we expect at Collection level. We don’t care for performances during discovery mode.

--- app/Http/Controllers/LinesController.php
+++ app/Http/Controllers/LinesController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Line;
-use Illuminate\Support\Facades\Storage;
 
 class LinesController extends Controller
 {
     public function __invoke()
     {
         $lines = Line::with('statuses')
-            ->orderBy('name')
             ->get()
+            ->sortBy('name')
             ->groupBy('name');
 
         return view('lines')->with('lines', $lines);
