d3ed09a54ce59cfcff269b1841f890e8ca781df1
Mehdi M <hi@mehdi.cc>
2024-05-12 00:27:35 +0200

    Add base CSS

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="color-scheme" content="dark light only">
     <title>@yield('title', 'STIB Open Data exploration')</title>
-    @vite('resources/css/app.css')
+    @vite([
+        'resources/css/base.css',
+        'resources/css/app.css',
+    ])
 </head>
 <body>
 
