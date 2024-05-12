1c1f9af2351c2da9d6342a5dc4c6de0e3168e4cd
Mehdi M <hi@mehdi.cc>
2024-05-12 14:46:25 +0200

    Draft presentation

--- resources/views/stib/subways.blade.php
+++ resources/views/stib/subways.blade.php
@@ -3,62 +3,99 @@
 @section('title', 'Subways – STIB Open Data exploration')
 
 @section('content')
-<h1>STIB Open Data exploration</h1>
 
-<p>Sample data from <a href="https://data.stib-mivb.brussels/explore/dataset/travellers-information-rt-production">Travellers Information (RT) on STIB open data portal</a></p>
+<ul class="lines">
+    <li class="lines__row line__rowCalendar">
+        <div class="line__name | flex-center"></div>
 
-<h2>Lines and stops</h2>
+        <div class="line__rowHours">
+            <div class="line__rowHour" style="--hour-index: 0;">0</div>
+            <div class="line__rowHour" style="--hour-index: 1;">1</div>
+            <div class="line__rowHour" style="--hour-index: 2;">2</div>
+            <div class="line__rowHour" style="--hour-index: 3;">3</div>
+            <div class="line__rowHour" style="--hour-index: 4;">4</div>
+            <div class="line__rowHour" style="--hour-index: 5;">5</div>
+            <div class="line__rowHour" style="--hour-index: 6;">6</div>
+            <div class="line__rowHour" style="--hour-index: 7;">7</div>
+            <div class="line__rowHour" style="--hour-index: 8;">8</div>
+            <div class="line__rowHour" style="--hour-index: 9;">9</div>
+            <div class="line__rowHour" style="--hour-index: 10;">10</div>
+            <div class="line__rowHour" style="--hour-index: 11;">11</div>
+            <div class="line__rowHour" style="--hour-index: 12;">12</div>
+            <div class="line__rowHour" style="--hour-index: 13;">13</div>
+            <div class="line__rowHour" style="--hour-index: 14;">14</div>
+            <div class="line__rowHour" style="--hour-index: 15;">15</div>
+            <div class="line__rowHour" style="--hour-index: 16;">16</div>
+            <div class="line__rowHour" style="--hour-index: 17;">17</div>
+            <div class="line__rowHour" style="--hour-index: 18;">18</div>
+            <div class="line__rowHour" style="--hour-index: 19;">19</div>
+            <div class="line__rowHour" style="--hour-index: 20;">20</div>
+            <div class="line__rowHour" style="--hour-index: 21;">21</div>
+            <div class="line__rowHour" style="--hour-index: 22;">22</div>
+            <div class="line__rowHour" style="--hour-index: 23;">23</div>
+        </div>
+    </li>
 
-<ul>
     @foreach ($lines as $line)
-        <h3>Line: {{ $line[0]->name }}</h3>
+    <li class="lines__row">
+        <h2 class="line__name | flex-center">{{ $line[0]->name }}</h2>
 
-        {{-- <ul>
-            <p>Direction {{ $line[0]->various->direction }}: <code>fr</code> {{ $line[0]->direction->fr }}, <code>nl</code> {{ $line[0]->direction->nl }}</p>
-            <p>Direction {{ $line[1]->various->direction }}: <code>fr</code> {{ $line[1]->direction->fr }}, <code>nl</code> {{ $line[1]->direction->nl }}</p>
-        </ul> --}}
+        <div class="line__rowDetail">
 
-        {{-- <h4>Status</h4> --}}
+            {{-- @todo: Show calendar of the day here. --}}
 
-        @if($line[0]->statuses->count())
+            {{-- <ul>
+                <p>Direction {{ $line[0]->various->direction }}: <code>fr</code> {{ $line[0]->direction->fr }}, <code>nl</code> {{ $line[0]->direction->nl }}</p>
+                <p>Direction {{ $line[1]->various->direction }}: <code>fr</code> {{ $line[1]->direction->fr }}, <code>nl</code> {{ $line[1]->direction->nl }}</p>
+            </ul> --}}
 
-        <ul>
-            @foreach($line[0]->statuses as $status)
-                <li>
-                    <h3>#{{ $loop->index }} at {{ $status->created_at }}</h3>
-                    {{-- <p><code>type</code>: {{ $status->type }}</p> --}}
-                    {{-- <p>Impacted lines: {{ $status->lines->pluck('name')->unique()->join(', ') }}</p> --}}
-                    <ul>
-                    @foreach ($status->content as $content)
-                        <li>
-                            <p><code>priority</code>: {{ $status->priority }}</p>
-                            {{-- <p><code>type</code>: {{ $content['type'] }}</p> --}}
+            {{-- <h4>Status</h4> --}}
 
-                            @foreach ($content['text'] as $text)
-                                @if(Arr::has($text, 'en'))
-                                    <p><code>en</code>: {{ $text['en'] }}</p>
-                                @endif
-                                @if(Arr::has($text, 'fr'))
-                                    <p><code>fr</code>: {{ $text['fr'] }}</p>
-                                @endif
-                                @if(Arr::has($text, 'nl'))
-                                    <p><code>nl</code>: {{ $text['nl'] }}</p>
-                                @endif
-                            @endforeach
-                        </li>
-                    @endforeach
-                    </ul>
-                </li>
-            @endforeach
-            </ul>
+            @if(!$line[0]->disruptions->count())
+                <p class="line__rowNoDisruption">The line runs fine today!</p>
+            @else
 
-        @else
+            <div class="line__rowHours line__rowStatus">
+                <button type="button" class="line__rowIncident" style="--hours: 10; --minutes: 25;"></button>
+            </div>
+            {{-- <ul>
+                @foreach($line[0]->disruptions as $disruption)
+                    <li>
+                        <h3>#{{ $loop->index }} at {{ $disruption->created_at }}</h3>
+                        <p><code>type</code>: {{ $disruption->type }}</p>
+                        <p>Impacted lines: {{ $disruption->lines->pluck('name')->unique()->join(', ') }}</p>
+                        <ul>
+                        @foreach ($disruption->content as $content)
+                            <li>
+                                <p><code>priority</code>: {{ $disruption->priority }}</p>
+                                <p><code>type</code>: {{ $content['type'] }}</p>
 
-        <p>Everything runs fine today!</p>
+                                @foreach ($content['text'] as $text)
+                                    @if(Arr::has($text, 'en'))
+                                        <p><code>en</code>: {{ $text['en'] }}</p>
+                                    @endif
+                                    @if(Arr::has($text, 'fr'))
+                                        <p><code>fr</code>: {{ $text['fr'] }}</p>
+                                    @endif
+                                    @if(Arr::has($text, 'nl'))
+                                        <p><code>nl</code>: {{ $text['nl'] }}</p>
+                                    @endif
+                                @endforeach
+                            </li>
+                        @endforeach
+                        </ul>
+                    </li>
+                @endforeach
+            </ul> --}}
 
-        @endif
+            @endif
+        </div>
 
+
+    </li>
     @endforeach
-
 </ul>
+
+<p>Sample data from <a class="underline" href="https://data.stib-mivb.brussels/explore/dataset/travellers-information-rt-production">real-time Travellers Information</a> (STIB)</p>
+
 @endsection
