158e62b20cdd64f0ce158e199497e3c4dae606fc
Mehdi M <hi@mehdi.cc>
2026-03-22 13:09:18 +0100

    Add new sample data

--- app/Console/Commands/StibSampleData.php
+++ app/Console/Commands/StibSampleData.php
@@ -28,9 +28,13 @@ class StibSampleData extends Command
             ->line('Feeding the database with sample STIB messages…');
 
         $data = collect([
+            // Former STIB API
             Storage::json('samples/2024-04-29-travellers-information-rt-production.json'),
             // Storage::json('samples/2024-05-10-travellers-information-rt-production.json'),
             // Storage::json('samples/2024-05-16-travellers-information-rt-production.json'),
+
+            // Current STIB API
+            Storage::json('samples/2026-03-22-travellers-information-rt-production.json')['results'],
         ])
             ->flatten(1);
 
